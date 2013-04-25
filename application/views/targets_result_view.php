2aa9334e308f565ca0604f73e0b1b3507ca80250
uciel_laptop <uciel@uciel-samsung.(none)>
2013-04-25 17:17:17 -0300

    jquery min changed

--- application/views/targets_result_view.php
+++ application/views/targets_result_view.php
@@ -36,7 +36,7 @@
   ?>
 </div >
 
-<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"> </script>
+<script src="http://code.jquery.com/jquery-1.9.1.min.js"> </script>
 
 <script>
 $('.starthidden').hide();
@@ -50,9 +50,5 @@ $(function(){
    "a.show",null);
 });
 </script>
-
-
-	
-
 
 
